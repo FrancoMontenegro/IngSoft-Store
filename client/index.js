ce6fb33821548d7841bbc77faee8a4ae618531d6
Vaaneamv <80649943+Vaaneamv@users.noreply.github.com>
2022-04-22 13:33:34 -0400

    Add files via upload

--- client/index.js
+++ client/index.js
@@ -1,1 +1,18 @@
-//LADO CLIENTE: CREAR INDEX Y VER COMO CONECTAR CON SERVER
+import React from "react";
+import ReactDOM from "react-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Auth0Provider
+      domain="vamv.us.auth0.com"
+      clientId="WV70pRbXsnXO7j3LVMPwD217oveX06WK"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
